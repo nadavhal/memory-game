2406f977766c85a9abb53c1f6e376bf387ed3975
nadav <nadavhaha@gmail.com>
2022-04-25 03:01:56 +0300

    mouse hover done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // variables:
 let srcCards = ["A", "B", "C", "D", "E", "F", "A", "B", "C", "D", "E", "F"];
-let len = srcCards.length;
-let handOfCards = srcCards;
+let numOfCards = srcCards.length;
+let handOfCards = srcCards; // הקלפים שעל השולחן
 let board = document.getElementById("game-table");
 let cardDiv;
 
@@ -9,28 +9,82 @@ let cardDiv;
 function shuffle(){
     let r = 0;
     let newHand = [];
-    for (n = 0; n < len; n++){
+    for (n = 0; n < numOfCards; n++){
         do {
-            r = Math.floor((Math.random() * len));
+            r = Math.floor((Math.random() * numOfCards));
         } while (newHand[r] != undefined);
         newHand[r] = srcCards[n];
     }
     return handOfCards = newHand;
 }
-function newGame(){
+
+let hover = (eve) => {
+    let x = eve.target.id;
+    document.getElementById(x).style.backgroundImage="url(cards/" + handOfCards[x] + ".png)"; 
+    console.log(eve.target);
+}
+let out = (even) => {
+    let y = even.target.id;
+    document.getElementById(y).style.backgroundImage="url(back.jpg)"; 
+}
+let cardClick = (event) => {
+    console.log(" the card is " + event.target.id);
+    // event.target.className = "cardBack";
+
+
+    //div = event.target;
+
+    //console.log (div);
+
+    //debugger;
+    
+}
+
+// function setCard(pos, type){        // draw 
+    
+//     switch (type) {
+//         case "A": 
+//         case "B": 
+//         case "C": 
+//         case "D":
+//         case "E":
+//         case "F": 
+//         document.getElementById(pos).style.backgroundImage="url(cards/" + type + ".png)"; 
+//         break;
+            
+//         case "back": 
+//         document.getElementById(pos).style.backgroundImage="url(back.jpg)"; 
+//         break;
+
+//         case "empty":
+//         default:
+//         document.getElementById(pos).style.backgroundImage=null;
+//         break;
+//     }
+// }
+
+function drawCards(){ 
+    for (n in handOfCards){
+        document.getElementById(n).style.backgroundImage="url(back.jpg)"; 
+    }
+}
+
+function newGame(){ // התחלת המשחק
     shuffle();
-    for(n of handOfCards){
+    drawCards();
+}
+
+function createCards(){ // יצירת מקומות הקלפים
+    for (n = 0; n < numOfCards; n++){
         cardDiv = document.createElement("div");
-        cardDiv.innerText = n;
-        cardDiv.className = "cardBack";
+        cardDiv.id = n;
+        cardDiv.className = "noCard";
+        cardDiv.onclick = cardClick;
+        cardDiv.onmouseout = out;
+        cardDiv.onmouseover = hover;
         board.appendChild(cardDiv);
     }
 }
 
-
-// play:
-// newGame();
-
-
-
-
+// init:
+createCards(); // אתחול תצוגת קלפים למצב מוסתר
